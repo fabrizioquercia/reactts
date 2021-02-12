583a72ccfc379aade02d76865d78a3caad6bf4eb
Fabrizio Quercia <fabrizio.quercia@gmail.com>
2021-02-12 17:33:50 +0100

    Prima bozza elenco (grezzo) navicelle (capsule)

--- src/Navicelle.tsx
+++ src/Navicelle.tsx
@@ -1,32 +1,35 @@
-import React, { Component } from 'react'
+import { useEffect, useState } from "react";
 
 function Navicelle() {
 
-    const boats: any[] = [];
-    
-        fetch('https://api.spacexdata.com/v3/capsules')
-        .then(res => res.json())
-        .then((data) => {
-            data({ boats: data })
-          })
-        .catch(console.log)
-  
+    const [spaceData, setSpaceData] = useState<any>();
 
+    useEffect(() => {
+        async function loadData(){
+          const resp = await fetch('https://api.spacexdata.com/v3/capsules');
+          const data = await resp.json();
+          if (data){
+            setSpaceData(data);
+          }
+        }
+        loadData()
+    },[])
+     
     return(
         <div>
-            <h2>Navicelle</h2>
-
-            {boats.map((boat) => (
-
-            <div className="card">navicelle...
-              <div className="card-body">
-                <h5 className="card-title">{boat.name}</h5>
-                <h6 className="card-subtitle mb-2 text-muted">{boat.email}</h6>
-                <p className="card-text">{boat.company.catchPhrase}</p>
-              </div>
-            </div>
-            ))}
+            <h2>Navicelle <em>(capsule)</em></h2>
+            
+            {spaceData?.map((item: any) => 
+                <div key={item.capsule_serial}><strong>{item.capsule_serial}:</strong> {item.details}
+                    <br />
+                    <p>Missioni:&nbsp;<br />
+                    {item.missions.map((m: any) =>
+                        <span>&nbsp;&nbsp; - name: {m.name} (volo {m.flight})<br/></span>
+                    )}
+                    </p>
+                </div>
+            )}
         </div>
     )
 }
-export default Navicelle;
+export default Navicelle; 
